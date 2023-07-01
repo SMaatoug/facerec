45f04159a3a72c112e690a0c56d9b1fcd5eac804
SMaatoug <71944764+SMaatoug@users.noreply.github.com>
2023-07-01 22:08:03 +0200

    Update face_rec.py

--- face_rec.py
+++ face_rec.py
@@ -1,16 +1,11 @@
 import cv2
-import dlib
-import face_recognition
 import streamlit as st
+from deepface import DeepFace
 from PIL import Image
 import numpy as np
 import os
 
-# Set up face detector and predictor (used for face landmarks)
-detector = dlib.get_frontal_face_detector()
-sp = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')  # provide path to your .dat file
-
-# Dictionary to store face encodings along with name
+# Dictionary to store face embeddings along with name
 known_faces = dict()
 
 def capture_images(name):
@@ -21,45 +16,31 @@ def capture_images(name):
         ret, frame = cap.read()
         # Convert to RGB
         frame = frame[:, :, ::-1]
-        # Get face encodings
-        faces = face_recognition.face_locations(frame)
-        if len(faces) == 0:
-            st.write("No face detected. Try again.")
-        else:
-            # Assuming only one face, get that face
-            top, right, bottom, left = faces[0]
-            face_image = frame[top:bottom, left:right]
-            images.append(face_image)
+        # Get face embeddings
+        resp_obj = DeepFace.analyze(frame, actions=['embedding'])
+        images.append(resp_obj["embedding"])
 
     cap.release()
     cv2.destroyAllWindows()
 
-    # Compute face encodings and store in known_faces
-    encodings = [face_recognition.face_encodings(img)[0] for img in images]
-    known_faces[name] = encodings
+    # Compute face embeddings and store in known_faces
+    known_faces[name] = images
 
 def identify_faces(frame):
-    # Detect faces
-    faces = face_recognition.face_locations(frame)
-    
-    for top, right, bottom, left in faces:
-        # Get face encodings
-        face_encodings = face_recognition.face_encodings(frame, [(top, right, bottom, left)])
+    # Get face embeddings
+    resp_obj = DeepFace.analyze(frame, actions=['embedding'])
 
-        name = 'Human'
-        for face_encoding in face_encodings:
-            # Compare with known faces
-            for known_name, known_encodings in known_faces.items():
-                matches = face_recognition.compare_faces(known_encodings, face_encoding)
-                if True in matches:
+    name = 'Human'
+    if "embedding" in resp_obj:
+        # Compare with known faces
+        for known_name, known_embeddings in known_faces.items():
+            for known_embedding in known_embeddings:
+                dist = np.linalg.norm(known_embedding - resp_obj["embedding"])
+                if dist < 0.1:  # threshold may need to be tuned
                     name = known_name
                     break
 
-            # Draw box and name
-            frame = cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-            frame = cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        
-    return frame
+    return name
 
 def main():
     st.title("Face Recognition App")
@@ -80,8 +61,8 @@ def main():
 
         while True:
             ret, frame = cap.read()
-            frame = identify_faces(frame)
-            placeholder.image(frame, channels="BGR")
+            name = identify_faces(frame)
+            placeholder.text(name)
 
         cap.release()
         cv2.destroyAllWindows()
